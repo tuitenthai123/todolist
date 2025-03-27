0d81dd85ae19a82bfd4226e84db893fae300bd3c
tuitenthai123 <21004148@st.vlute.edu.vn>
2025-03-28 00:55:13 +0700

    first-commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from "vuex"
+import vuetify from './plugins/vuetify'
+
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state:{
+    conmeo:0
+  }
+})
 
 new Vue({
-  render: h => h(App),
+  store,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
